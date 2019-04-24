a83caf9825262f87e6315d7cc74d07141321e57d
Anonymous <Anonymous@Anonymous.Com>
2019-04-24 23:01:18 +0300

    Add main idea.

---
+++ Pinger/Pinger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace Pinger
+{
+    public class Pinger
+    {
+        public Dictionary<string, string> Ping(List<string> rowhosts)
+        {
+            Dictionary<string, string> answer = new Dictionary<string, string>();
+            Ping ping = new Ping();
+            foreach (var rowhost in rowhosts)
+            {
+                try
+                {
+                    PingReply pingReply = ping.Send(rowhost);
+                    answer.Add(pingReply.Address.ToString(), pingReply.Status.ToString());
+                }
+                catch (PingException e)
+                {
+                    Console.Write(DateTime.Now + " " + e.Message);
+                }            
+            }
+            return answer;
+        }
+
+        public void PingAndLogging (Dictionary<string, string> pingedhosts, string logpath)
+        {
+            Ping ping = new Ping();
+
+            foreach (var pingedhost in pingedhosts)
+            {
+                try
+                {
+                    PingReply pingReply = ping.Send(pingedhost.Key);
+                    if (pingReply.Status.ToString() != pingedhost.Value)
+                    {
+                        using (var writer = new StreamWriter(logpath, true))
+                        {
+                            writer.WriteLine(DateTime.Now + " " + pingReply.Address + " " + pingReply.Status);
+                        }
+                    }
+                }
+                catch (ArgumentException e)
+                {
+                    using (var writer = new StreamWriter(logpath, true))
+                    {
+                        Console.WriteLine(DateTime.Now + " " + e.Message);
+                    }
+                }
+
+            }
+            
+        }
+    }
+}
--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Pinger
 {
@@ -7,6 +9,21 @@ namespace Pinger
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var hosts = File.ReadAllLines("./hosts.txt");
+            List<string> hostslist = new List<string>(hosts);
+            Pinger pinger = new Pinger();
+            Dictionary<string, string> hostcollection = new Dictionary<string, string>();
+            hostcollection = pinger.Ping(hostslist);
+            foreach (var host in hostcollection)
+            {
+                Console.WriteLine("Host: " + host.Key);
+                Console.WriteLine("Status: " + host.Value);
+            }
+
+
+            pinger.PingAndLogging(hostcollection, "./log.txt");
+
+            Console.ReadKey();
         }
     }
 }
