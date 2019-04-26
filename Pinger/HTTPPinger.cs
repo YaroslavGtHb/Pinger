883bdfda90ca8f9a036bba047df1de6372b2d425
HubForEducation <hubforeducation@yandex.ru>
2019-04-26 17:43:06 +0300

    Add first part of HTTP.

--- Pinger/HTTPPinger.cs
+++ Pinger/HTTPPinger.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
 
 namespace Pinger
 {
@@ -6,7 +8,29 @@ namespace Pinger
     {
         public Dictionary<string, string> Ping(List<string> rowhosts)
         {
-            
+            Dictionary<string, string> answer = new Dictionary<string, string>();
+            Ping ping = new Ping();
+            foreach (var rowhost in rowhosts)
+            {
+                try
+                {
+                    HttpWebRequest webRequest = (HttpWebRequest)WebRequest
+                        .Create(rowhost);
+                    webRequest.AllowAutoRedirect = false;
+                    HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+                    
+                    if (response.StatusCode.ToString() != null) answer.Add(rowhost, "OK (" + response.StatusCode.ToString() + ")");
+                }
+                catch (PingException)
+                {
+                    answer.Add(rowhost, "FAILED");
+                }            
+            }
+            return answer;          
         }
+        
+        
+        
+        
     }
 }
