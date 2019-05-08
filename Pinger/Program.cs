da56f5023105f2e6a823bf133ab9776792b3bda0
Anonymous <Anonymous@Anonymous.Com>
2019-05-08 11:31:37 +0300

    TCPPinger.

--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Pinger
 {
@@ -24,15 +29,42 @@ namespace Pinger
             //Console.ReadKey();
 
 
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest
-                .Create("http://google.com");
-            webRequest.AllowAutoRedirect = false;
-            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
-            //http://qaru.site/questions/30019/getting-http-status-code-number-200-301-404-etc-from-httpwebrequest-and-httpwebresponse
-            //https://stackoverflow.com/questions/26067342/how-to-implement-psping-tcp-ping-in-c-sharp
+            //HttpWebRequest webRequest = (HttpWebRequest)WebRequest
+            //    .Create("http://google.com");
+            //webRequest.AllowAutoRedirect = false;
+            //HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+            ////http://qaru.site/questions/30019/getting-http-status-code-number-200-301-404-etc-from-httpwebrequest-and-httpwebresponse
+            ////https://stackoverflow.com/questions/26067342/how-to-implement-psping-tcp-ping-in-c-sharp
 
 
-            Console.Write(response.StatusCode.ToString());
+            //Console.Write(response.StatusCode.ToString());
+
+             
+            IPAddress[] ip = Dns.GetHostAddresses("google123.com");
+            //System.Net.Sockets.SocketException: "Этот хост неизвестен"
+            EndPoint endPoint = new IPEndPoint(ip[0], 80);
+            var times = new List<double>();
+            for (int i = 0; i < 4; i++)
+            {
+                var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                sock.Blocking = true;
+
+                var stopwatch = new Stopwatch();
+
+                // Measure the Connect call only
+                stopwatch.Start();
+                sock.Connect(endPoint);
+                stopwatch.Stop();
+
+                double t = stopwatch.Elapsed.TotalMilliseconds;
+                Console.WriteLine("{0:0.00}ms", t);
+                times.Add(t);
+
+                sock.Close();
+
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("{0:0.00} {1:0.00} {2:0.00}", times.Min(), times.Max(), times.Average());
 
         }
     }
---
+++ Pinger/TCPPinger.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Pinger
+{
+    public class TCPPinger
+    {
+        Dictionary<string, string> Ping(List<string> rowhosts)
+        {
+            Dictionary<string, string> answer = new Dictionary<string, string>();
+
+
+            foreach (var rowhost in rowhosts)
+            {
+                try
+                {
+                    IPAddress[] ip = Dns.GetHostAddresses(rowhost);
+
+                    EndPoint endPoint = new IPEndPoint(ip[0], 80);
+                    var times = new List<double>();
+                    for (int i = 0; i < 4; i++)
+                    {
+                        var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        sock.Blocking = true;
+
+                        var stopwatch = new Stopwatch();
+
+                        stopwatch.Start();
+                        sock.Connect(endPoint);
+                        stopwatch.Stop();
+
+                        double t = stopwatch.Elapsed.TotalMilliseconds;
+                        times.Add(t);
+
+                        sock.Close();
+
+                        Thread.Sleep(1000);
+                    }
+
+                    answer.Add(rowhost, "OK (" + "MinTime: " + times.Min() + "MaxTime: " + times.Max() + "Average: " + times.Average() + " " + ")");
+                }
+                catch (SocketException)
+                {
+                    answer.Add(rowhost, "FAILED");
+                }
+            }
+            return answer;
+        }
+
+        public void PingAndLogging(Dictionary<string, string> pingedhosts, string logpath)
+        {
+            Ping ping = new Ping();
+
+            foreach (var pingedhost in pingedhosts)
+            {
+                try
+                {
+                    IPAddress[] ip = Dns.GetHostAddresses(pingedhost.Key);
+
+                    EndPoint endPoint = new IPEndPoint(ip[0], 80);
+                    var times = new List<double>();
+                    for (int i = 0; i < 4; i++)
+                    {
+                        var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        sock.Blocking = true;
+
+                        var stopwatch = new Stopwatch();
+
+                        stopwatch.Start();
+                        sock.Connect(endPoint);
+                        stopwatch.Stop();
+
+                        double t = stopwatch.Elapsed.TotalMilliseconds;
+                        times.Add(t);
+
+                        sock.Close();
+
+                        Thread.Sleep(1000);
+                    }
+                        using (var writer = new StreamWriter(logpath, true))
+                        {
+                            writer.WriteLine(DateTime.Now + "OK (" + "MinTime: " + times.Min() + "MaxTime: " + times.Max() + "Average: " + times.Average() + " " + ")");
+                        }
+                }
+                catch (SocketException)
+                {
+                    using (var writer = new StreamWriter(logpath, true))
+                    {
+                        writer.WriteLine(DateTime.Now + " " + pingedhost.Key + " " + "FAILED");
+                    }
+                }
+
+            }
+        }
+    }
+}
