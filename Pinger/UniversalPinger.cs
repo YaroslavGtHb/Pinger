eee8f3c52693d6f28232ebd97a41a9cb3ef76310
Anonymous <Anonymous@Anonymous.Com>
2019-05-10 00:13:53 +0300

    TODO.

--- Pinger/UniversalPinger.cs
+++ Pinger/UniversalPinger.cs
@@ -13,6 +13,10 @@
             _TcpPinger = TcpPinger;
         }
 
+        //TODO add logic to UniversalPinger.
+        //TODO fix all pinger classes to new format, one method for check and write to file, one metod for check one ip.
+        //https://habr.com/ru/post/131993/
+        //http://80levelelf.com/Post?postId=20
 
     }
 }
