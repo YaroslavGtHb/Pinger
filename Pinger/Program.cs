a797c6e8b51c14cd7398b326383eb2f3d00297bc
Anonymous <Anonymous@Anonymous.Com>
2019-05-12 22:46:32 +0300

    Add containers. (NEED FIX).

--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.IO.Pipes;
 using Ninject;
 using Ninject.Modules;
+using Ninject.Parameters;
 
 namespace Pinger
 {
@@ -15,7 +16,8 @@ namespace Pinger
 
             IKernel kernel = new StandardKernel(new NinjectConfig());
 
-            UniversalPinger pinger = kernel.Get<UniversalPinger>();
+            UniversalPinger pinger = kernel.Get<UniversalPinger>(new Parameter("rowhosts", hosts, true));
+            pinger.Run();
         }
         //https://habr.com/ru/post/235995/
         //http://codeclimber.net.nz/archive/2009/02/05/how-to-use-ninject-with-aspnet-mvc/
