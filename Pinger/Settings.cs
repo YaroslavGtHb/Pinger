41189116697d1b1957f58297ad3cc7f1676191e4
Anonymous <Anonymous@Anonymous.Com>
2019-05-22 08:21:18 +0300

    Add settings class.

---
+++ Pinger/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pinger
+{
+    class Settings
+    {
+        public string rowhosts { get; set; }
+        public string logpath { get; set; }
+        public string protocol { get; set; }
+        public int httpvalidcode { get; set; }
+    }
+}
