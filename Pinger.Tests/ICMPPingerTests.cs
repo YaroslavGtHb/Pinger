84e76b11f3cb3695a91485595a7cd2c6d994dec8
Anonymous <Anonymous@Anonymous.Com>
2019-05-29 08:29:05 +0300

    FIRSTTESTADDEDED!

--- Pinger.Tests/ICMPPingerTests.cs
+++ Pinger.Tests/ICMPPingerTests.cs
@@ -13,31 +13,6 @@ namespace Pinger.Tests
         }
 
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-
-        [Test]
-        public void Ping_AviableHost_And_AviableLogpath_Expects_Aviablehost_And_OK()
-        {
-            List<string> hosts = new List<string>();
-            string host = "https://google.com/";
-            hosts.Add(host);
-
-            string logpath = "./logstest.txt";
-
-            ICMPPinger icmppinger = new ICMPPinger(hosts, logpath);
-
-            Dictionary<string, string> actual = new Dictionary<string, string>();
-            actual.Add(host, "FAILED");
-
-            var expected = icmppinger.Ping();
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
         public void PingTest()
         {
             string logpath = "./LogsTest.txt";
@@ -49,9 +24,9 @@ namespace Pinger.Tests
 
             Dictionary<string, string> expected = icmppinger.Ping();
 
-            actual.Add("https://www.google.com/", "OK");
+            actual.Add("https://www.google.com/", "FAILED");
             actual.Add("https://www.google1234455435435.com/", "FAILED");
-            actual.Add("92.51.57.80", "FAILED");
+            actual.Add("92.51.57.80", "OK");
             actual.Add("34.22.1.23", "FAILED");
 
             Assert.AreEqual(expected, actual);
